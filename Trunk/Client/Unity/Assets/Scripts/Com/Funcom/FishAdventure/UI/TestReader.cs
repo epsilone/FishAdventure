46ded81563797f150d06d6485e3c737ca4161081
Benjamin Bounous <benjaminb@funcom.com>
2013-02-25 15:31:48 -0500

    adding a new fish template

--- Trunk/Client/Unity/Assets/Scripts/Com/Funcom/FishAdventure/UI/TestReader.cs
+++ Trunk/Client/Unity/Assets/Scripts/Com/Funcom/FishAdventure/UI/TestReader.cs
@@ -7,9 +7,11 @@ public class TestReader:MonoBehaviour
 	public AnimationClip animationBuffer;
 	private GameObject Fish;
 	
+	private const int NUMBER_OF_BUTTON = 5;
+	
 	void OnGUI()
 	{
-		if (GUI.Button(new Rect(Screen.width-100,((Screen.height / 5) * 1) - 15,100,30),"Mario"))
+		if (GUI.Button(new Rect(Screen.width-100,((Screen.height / (NUMBER_OF_BUTTON + 1)) * 1) - 15,100,30),"Mario"))
 		{
 			if(Fish != null)
 			{
@@ -30,7 +32,7 @@ public class TestReader:MonoBehaviour
 		    fishGameObject.animation.Play("idle");*/
 		}
 		
-		if (GUI.Button(new Rect(Screen.width-100,((Screen.height / 5) * 2) - 15,100,30),"Block"))
+		if (GUI.Button(new Rect(Screen.width-100,((Screen.height / (NUMBER_OF_BUTTON + 1)) * 2) - 15,100,30),"Block"))
 		{
 			if(Fish != null)
 			{
@@ -51,7 +53,7 @@ public class TestReader:MonoBehaviour
 		    fishGameObject.animation.Play("idle");*/
 		}
 		
-		if (GUI.Button(new Rect(Screen.width-100,((Screen.height / 5) * 3) - 15,100,30),"Bus"))
+		if (GUI.Button(new Rect(Screen.width-100,((Screen.height / (NUMBER_OF_BUTTON + 1)) * 3) - 15,100,30),"Bus"))
 		{
 			if(Fish != null)
 			{
@@ -72,7 +74,7 @@ public class TestReader:MonoBehaviour
 		    fishGameObject.animation.Play("idle");*/
 		}
 		
-		if (GUI.Button(new Rect(Screen.width-100,((Screen.height / 5) * 4) - 15,100,30),"Octopod"))
+		if (GUI.Button(new Rect(Screen.width-100,((Screen.height / (NUMBER_OF_BUTTON + 1)) * 4) - 15,100,30),"Octopod"))
 		{
 			if(Fish != null)
 			{
@@ -80,6 +82,27 @@ public class TestReader:MonoBehaviour
 			}
 			
             Fish = LEGOXMLReader.GetLegoObjectByLxfml((Resources.Load("octopod", typeof(TextAsset)) as TextAsset).text, "Fish");
+			Fish.AddComponent("ViewDrag");
+			//GenericUtil.CombineChildMeshes(Fish);
+			//GenericUtil.CreateBones(Fish);
+			//GenericUtil.SkinIt(Fish, 0.10f);
+			//GenericUtil.Center(Fish);
+			
+			//Animation
+			/*fishGameObject.AddComponent<Animation>();
+			fishGameObject.animation.AddClip(animationBuffer, "idle");
+			fishGameObject.animation.wrapMode = WrapMode.Loop;
+		    fishGameObject.animation.Play("idle");*/
+		}
+		
+		if (GUI.Button(new Rect(Screen.width-100,((Screen.height / (NUMBER_OF_BUTTON + 1)) * 5) - 15,100,30),"horizontal"))
+		{
+			if(Fish != null)
+			{
+				Destroy(Fish);
+			}
+			
+            Fish = LEGOXMLReader.GetLegoObjectByLxfml((Resources.Load("horizontalfish", typeof(TextAsset)) as TextAsset).text, "Fish");
 			Fish.AddComponent("ViewDrag");
 			//GenericUtil.CombineChildMeshes(Fish);
 			//GenericUtil.CreateBones(Fish);
