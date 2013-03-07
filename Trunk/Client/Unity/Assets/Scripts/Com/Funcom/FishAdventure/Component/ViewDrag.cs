630b7dc3ec1f749e4b5da1d1ac1c65d71bc6fb44
Michael Tavares <michaelt@funcom.com>
2013-03-07 11:02:10 -0500

    Adjustment to view drag to ignore zoom pinch

--- Trunk/Client/Unity/Assets/Scripts/Com/Funcom/FishAdventure/Component/ViewDrag.cs
+++ Trunk/Client/Unity/Assets/Scripts/Com/Funcom/FishAdventure/Component/ViewDrag.cs
@@ -1,35 +1,37 @@
 using UnityEngine;
 using System.Collections;
 
-public class ViewDrag : MonoBehaviour 
-{
-	Vector3 hit_position = Vector3.zero;
-	Vector3 current_position = Vector3.zero;
-	
-	void Start () 
-	{
-	
-	}
-	
-	void Update()
-	{
-	    if(Input.GetMouseButtonDown(0))
-		{
-	        hit_position = Input.mousePosition;
-	
-	    }
-	    if(Input.GetMouseButton(0))
-		{
-	        current_position = Input.mousePosition;
-	        LeftMouseDrag();        
-	    }
-	}
-	
-	void LeftMouseDrag()
-	{
-	    Vector3 position = hit_position - current_position;
-		Vector3 invert = new Vector3(0, position.x, 0);
-		transform.Rotate(invert);
-		hit_position = current_position;
-	}
+public class ViewDrag : MonoBehaviour {
+    private Vector3 hit_position = Vector3.zero;
+    private Vector3 current_position = Vector3.zero;
+
+    private void Start() {
+
+    }
+
+    private void Update() {
+        if (Input.touchCount == 2)
+            return;
+
+        if (Input.GetMouseButtonDown(0)) {
+            hit_position = Input.mousePosition;
+
+        }
+
+        if (Input.GetMouseButton(0)) {
+            current_position = Input.mousePosition;
+            LeftMouseDrag();
+        }
+
+    }
+
+
+
+
+    void LeftMouseDrag() {
+        Vector3 position = hit_position - current_position;
+        Vector3 invert = new Vector3(0, position.x, 0);
+        transform.Rotate(invert);
+        hit_position = current_position;
+    }
 }
